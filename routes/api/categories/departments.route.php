10a12e4106c87d907c576f658bc78bfbfa5ca118
silah <silakosy@gmail.com>
2024-01-06 16:15:13 +0300

    update prefer stable to true

--- routes/api/categories/departments.route.php
+++
@@ -1,6 +0,0 @@
-<?php
-$controller = \App\Http\Controllers\Api\Departments\DepartmentsController::class;
-Route::controller($controller)->group(function () {
-        Route::post('store', 'store')
-            ->whereIn('roles', ['user', 'admin', 'client']);
-});
--- routes/api/users/users.route.php
+++
@@ -1,4 +0,0 @@
-<?php
-Route::controller(App\Http\Controllers\Api\Auth\AuthControllers::class)->group(function () {
-        Route::get('auth/user', 'getUser')->name('index');
-});
